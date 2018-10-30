99499388ef314076a5757788fdec4af2d0969914
马玉峰 <mayufeng@yuMac.local>
2018-10-30 19:51:05 +0800

    update qshell

--- daily-tools/merge_and_split.sh
+++ daily-tools/merge_and_split.sh
@@ -7,7 +7,7 @@ python ronghe.py $dir_a $dir_b $output
 
 sh split_date.sh $output $label
 
-qshell account XBhrm_cIe71aI4HZPj7oBZm-uYPa4D3pzVfNw5LB -2uQxaJFqFcCKUjyCvEkvphzK5JOxakj5aWXyi6u
+qshell account 
 
 for file in `ls $label/`
 do
